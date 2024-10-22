1de7e70524355eab2616d7afcd6c47e01d115158
Aleksey Alekseev <a.alekseev@rdp.ru>
2024-10-22 21:44:12 +0300

    add api

--- opinions_app/__init__.py
+++ opinions_app/__init__.py
@@ -6,8 +6,8 @@ from settings import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# app.json.ensure_ascii = False
+app.json.ensure_ascii = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from . import cli_commands, error_handlers, views
+from opinions_app import api_views, cli_commands, error_handlers, views
--- opinions_app/views.py
+++ opinions_app/views.py
@@ -7,13 +7,19 @@ from opinions_app.forms import OpinionForm
 from opinions_app.models import Opinion
 
 
+def random_opinion():
+    quantity = Opinion.query.count()
+    if quantity:
+        offset_value = randrange(quantity)
+        opinion = Opinion.query.offset(offset_value).first()
+        return opinion
+
+
 @app.route('/')
 def index_view():
-    quantity = Opinion.query.count()
-    if not quantity:
+    opinion = random_opinion()
+    if opinion is None:
         abort(500)
-    offset_value = randrange(quantity)
-    opinion = Opinion.query.offset(offset_value).first()
     return render_template('opinion.html', opinion=opinion)
 
 
